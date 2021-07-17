ee89df9dfcc81fd2f724d38380c807bf6aac3e27
gauravbasu6 <gauravbasu6@gmail.com>
2021-07-17 15:48:22 +0530

    Added all port scan

--- PortScanner.py
+++ PortScanner.py
@@ -1,5 +1,13 @@
 import nmap
 
+def callback_result(host, scan_result):
+    print('*'*50)
+    for port in scan_result['scan'][ipaddr]['tcp']:
+        if scan_result['scan'][ipaddr]['tcp'][port]['name'] == '' :
+            print(f"Port : {port}\tService : unknown\tState : {scan_result['scan'][ipaddr]['tcp'][port]['state']}")
+        else:
+            print(f"Port : {port}\tService : {scan_result['scan'][ipaddr]['tcp'][port]['name']}\tState : {scan_result['scan'][ipaddr]['tcp'][port]['state']}")
+
 print('*'*50)
 print(' '*12+'python-nmap port scanner')
 print('*'*50)
@@ -7,10 +15,12 @@ ipaddr = input('Enter IP address:')
 print('*'*50)
 print('Options:')
 print('1 : Scan the important ports (0-1000)')
-# print('2 : Scan all 65536 ports')
+print('2 : Scan all 65536 ports')
 
 
 nm = nmap.PortScanner()
+nma = nmap.PortScannerAsync()
+
 counter = 0
 
 while counter == 0:
@@ -20,40 +30,45 @@ while counter == 0:
 
     if choice == '1':
         ports = '0-1000'
-    # elif choice == '2':
-    #     ports = '0-65535'
-    else:
-        print('Incorrect choice! Enter again : ')
-        continue
+        print(f'Scanning ports {ports} on {ipaddr}')
+        nm.scan(ipaddr, ports)
+        print('*'*50)
+        print(nm.command_line())    
+        #TCP
+        if 'tcp' in nm[ipaddr].all_protocols():
+            print('Protocol : TCP')
+            for port in nm[ipaddr].all_tcp() :
+                print(f"Port : {port}\tService : {nm[ipaddr]['tcp'][port]['name']}\tState : {nm[ipaddr]['tcp'][port]['state']}")
+            
+        #UDP
+        if 'udp' in nm[ipaddr].all_protocols():
+            print('Protocol : UDP')
+            for port in nm[ipaddr].all_udp() :
+                print(f"Port : {port}\tService : {nm[ipaddr]['udp'][port]['name']}\tState : {nm[ipaddr]['udp'][port]['state']}")
 
-    print(f'Scanning ports {ports} on {ipaddr}')
-    nm.scan(ipaddr, ports)
-    print('*'*50)
-    print(nm.command_line())    
-    #TCP
-    if 'tcp' in nm[ipaddr].all_protocols():
-        print('Protocol : TCP')
-        for port in nm[ipaddr].all_tcp() :
-            print(f"Port : {port}\tService : {nm[ipaddr]['tcp'][port]['name']}\tState : {nm[ipaddr]['tcp'][port]['state']}")
+        #IP
+        if 'ip' in nm[ipaddr].all_protocols():
+            print('Protocol : IP')
+            for port in nm[ipaddr].all_ip() :
+                print(f"Port : {port}\tService : {nm[ipaddr]['ip'][port]['name']}\tState : {nm[ipaddr]['ip'][port]['state']}")
         
-    #UDP
-    if 'udp' in nm[ipaddr].all_protocols():
-        print('Protocol : UDP')
-        for port in nm[ipaddr].all_udp() :
-            print(f"Port : {port}\tService : {nm[ipaddr]['udp'][port]['name']}\tState : {nm[ipaddr]['udp'][port]['state']}")
+        #SCTP
+        if 'sctp' in nm[ipaddr].all_protocols():
+            print('Protocol : SCTP')
+            for port in nm[ipaddr].all_sctp() :
+                print(f"Port : {port}\tService : {nm[ipaddr]['sctp'][port]['name']}\tState : {nm[ipaddr]['sctp'][port]['state']}")
 
-    #IP
-    if 'ip' in nm[ipaddr].all_protocols():
-        print('Protocol : IP')
-        for port in nm[ipaddr].all_ip() :
-            print(f"Port : {port}\tService : {nm[ipaddr]['ip'][port]['name']}\tState : {nm[ipaddr]['ip'][port]['state']}")
+    elif choice == '2' :
+        ports = '0-65535'
+        print(f'Scanning ports {ports} on {ipaddr}')
+        nma.scan(hosts=ipaddr,arguments='-p 0-65535',callback=callback_result)
+        while nma.still_scanning():
+            nma.wait(2)
     
-    #SCTP
-    if 'sctp' in nm[ipaddr].all_protocols():
-        print('Protocol : SCTP')
-        for port in nm[ipaddr].all_sctp() :
-            print(f"Port : {port}\tService : {nm[ipaddr]['sctp'][port]['name']}\tState : {nm[ipaddr]['sctp'][port]['state']}")
-
+    else:
+        print('Incorrect choice. Enter again : ')
+        continue
+    
     counter = 1
 
 print('*'*50)
